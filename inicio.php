12ac0bc619424a785b0f59681c91d4f263d80326
BrigitteJos <70828263@unitekpuno.edu.pe>
2018-08-20 19:01:35 -0500

    estructura basica

---
+++ inicio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi Examen</title>
+</head>
+<body>
+	<h1>Hola Bienvenido a mi Pagina</h1><br>
+	<h2>Mi examen :'v</h2><br>
+	<h3>Presentado por: Brigitte Escobedo</h3><br>
+
+</body>
+</html>
