5e49b030301ebd12e880096fd352bc69863784a7
BrigitteJos <70828263@unitekpuno.edu.pe>
2018-08-20 19:22:42 -0500

    agregando color

--- inicio.php
+++ inicio.php
@@ -4,9 +4,9 @@
 	<title>Mi Examen</title>
 </head>
 <body>
-	<h1>Hola Bienvenido a mi Pagina</h1><br>
-	<h2>Mi examen :'v</h2><br>
+	<h1>Hola Bienvenido a mi Pagina</h1>
+	<center><font color=red><h2>Mi examen :'v</h2></font></center><br>
 	<h3>Presentado por: Brigitte Escobedo</h3><br>
-
+		<img src="torre.jpg">
 </body>
 </html>
